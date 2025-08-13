99e180f06657d1882448d8a49afadc282b132863
DragonzKilla <taichik.2001@gmail.com>
2025-08-13 10:15:08 -0400

    Add files via upload

---
+++ src/js/data/2025-08-12.js
@@ -0,0 +1,250 @@
+dataSetVersion = "2025-08-12"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [
+  {
+    name: "Filter by Branch",
+    key: "branch",
+    tooltip: "Check this to restrict to certain series.",
+    checked: false,
+    sub: [
+      { name: "JP", key: "JP" },
+      { name: "EN", key: "EN" },
+      { name: "CN", key: "CN" },
+    ]
+  }
+];
+
+dataSet[dataSetVersion].characterData = [
+  {
+    name: "Kaga Sumire",
+    img: "kaga_sumire.png",
+    opts: {
+      branch: ["JP"]
+    }
+  },
+  {
+    name: "Kaga Nazuna",
+    img: "kaga_nazuna.png",
+    opts: {
+      branch: ["JP"]
+    }
+  },
+  {
+    name: "Kogara Toto",
+    img: "kogara_toto.png",
+    opts: {
+      branch: ["JP"]
+    }
+  },
+  {
+    name: "Ichinose Uruha",
+    img: "ichinose_uruha.png",
+    opts: {
+      branch: ["JP"]
+    },
+  },
+  {
+    name: "Kurumi Noah",
+    img: "kurumi_noah.png",
+    opts: {
+      branch: ["JP"]
+    }
+  },
+  {
+    name: "Tosaki Mimi",
+    img: "tosaki_mimi.png",
+    opts: {
+      branch: ["JP"]
+    }
+  },
+  {
+    name: "Asumi Sena",
+    img: "asumi_sena.png",
+    opts: {
+      branch: ["JP"]
+    }
+  },
+  {
+    name: "Tachibana Hinano",
+    img: "tachibana_hinano.png",
+    opts: {
+      branch: ["JP"]
+    }
+  },
+  {
+    name: "Hanabusa Lisa",
+    img: "hanabusa_lisa.png",
+    opts: {
+      branch: ["JP"]
+    }
+  },
+  {
+    name: "Kisaragi Ren",
+    img: "kisaragi_ren.png",
+    opts: {
+      branch: ["JP"]
+    }
+  },
+  {
+    name: "Kaminari Qpi",
+    img: "kaminari_qpi.png",
+    opts: {
+      branch: ["JP"]
+    }
+  },
+  {
+    name: "Yakumo Beni",
+    img: "yakumo_beni.png",
+    opts: {
+      branch: ["JP"]
+    }
+  },
+  {
+    name: "Aizawa Ema",
+    img: "aizawa_ema.png",
+    opts: {
+      branch: ["JP"]
+    }
+  },
+  {
+    name: "Shinomiya Runa",
+    img: "shinomiya_runa.png",
+    opts: {
+      branch: ["JP"]
+    }
+  },
+  {
+    name: "Nekota Tsuna",
+    img: "nekota_tsuna.png",
+    opts: {
+      branch: ["JP"]
+    }
+  },
+  {
+    name: "Shiranami Ramune",
+    img: "shiranami_ramune.png",
+    opts: {
+      branch: ["JP"]
+    }
+  },
+  {
+    name: "Komori Met",
+    img: "komori_met.png",
+    opts: {
+      branch: ["JP"]
+    }
+  },
+  {
+    name: "Yumeno Akari",
+    img: "yumeno_akari.png",
+    opts: {
+      branch: ["JP"]
+    }
+  },
+  {
+    name: "Yano Kuromu",
+    img: "yano_kuromu.png",
+    opts: {
+      branch: ["JP"]
+    }
+  },
+  {
+    name: "Tsumugi Kokage",
+    img: "tsumugi_kokage.png",
+    opts: {
+      branch: ["JP"]
+    }
+  },
+  {
+    name: "Sendo Yuuhi",
+    img: "sendo_yuuhi.png",
+    opts: {
+      branch: ["JP"]
+    }
+  },
+  {
+    name: "Choya Hanabi",
+    img: "choya_hanabi.png",
+    opts: {
+      branch: ["JP"]
+    }
+  },
+  {
+    name: "Amayui Moka",
+    img: "amayui_moka.png",
+    opts: {
+      branch: ["JP"]
+    }
+  },
+  {
+    name: "Remia Aotsuki",
+    img: "remia_aotsuki.png",
+    opts: {
+      branch: ["EN"]
+    }
+  },
+  {
+    name: "Arya Kuroha",
+    img: "arya_kuroha.png",
+    opts: {
+      branch: ["EN"]
+    }
+  },
+  {
+    name: "Jira Jisaki",
+    img: "jira_jisaki.png",
+    opts: {
+      branch: ["EN"]
+    }
+  },
+  {
+    name: "Narin Mikure",
+    img: "narin_mikure.png",
+    opts: {
+      branch: ["EN"]
+    }
+  },
+  {
+    name: "Riko Solari",
+    img: "riko_solari.png",
+    opts: {
+      branch: ["EN"]
+    }
+  },
+  {
+    name: "Aya Kohari",
+    img: "aya_kohari.png",
+    opts: {
+      branch: ["CN"]
+    }
+  },
+  {
+    name: "Tsuyuri Shirasaki",
+    img: "tsuyuri_shirasaki.png",
+    opts: {
+      branch: ["CN"]
+    }
+  },
+  {
+    name: "Pafi",
+    img: "pafi.png",
+    opts: {
+      branch: ["CN"]
+    }
+  },
+  {
+    name: "Sen",
+    img: "sen.png",
+    opts: {
+      branch: ["CN"]
+    }
+  },
+  {
+    name: "Hinata Haru",
+    img: "hinata_haru.png",
+    opts: {
+      branch: ["CN"]
+    }
+  }
+];
